25d6c05792a5785fa8508d76433231ca0ea4d414
Alexander Uchitel <ltr4alex@yahoo.com>
2020-02-08 13:57:52 -0800

    Navigation warnings

--- app/src/main/java/utils/GlobalUtils.java
+++ app/src/main/java/utils/GlobalUtils.java
@@ -136,12 +136,17 @@ public class GlobalUtils {
 
     // ===================================================
     public static void hideSystemUI(@NonNull View view) {
-        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            flags = flags | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            flags = flags | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+        view.setSystemUiVisibility(flags);
     }
 
     // ===================================================
